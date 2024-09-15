dd1674f6e9ca27a2e9ca74048422fbd10b422896
Ruth Wata <88207822+r-n-wata@users.noreply.github.com>
2024-09-14 22:33:35 -0500

    change gmail config

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const port = 3000;
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "ruthnwata@gmail.com",
-    pass: "nhfe boxi sxqg qawl",
+    user: "outretreats@gmail.com",
+    pass: "zjum nyey smcc fcvj",
   },
 });
 
 app.get("/send-test-email", async (req, res) => {
   const mailOptions = {
     from: req.body.from || "",
-    to: "ruthnwata@gmail.com",
+    to: "outretreats@gmail.com",
     subject: req.body.subject,
     text: req.body.text,
   };
